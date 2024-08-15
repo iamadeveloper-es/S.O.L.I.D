a897684441abffbd66439ea647c584e56fc40300
Daniel Verdu <iamadeveloper.es@gmail.com>
2024-08-15 06:32:14 +0200

    Inicio proyecto

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// import './01-Srp/index'
+// import './01-Srp/refactor'
+
+// import './02-Oc/index'
+// import './02-Oc/refactor'
+
+// import './03-Liskov/index-a'
+import './03-Liskov/refactor-a'
